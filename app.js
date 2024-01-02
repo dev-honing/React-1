6cb7609163fb5e0347f3aa3a3cd001bbb4e1d906
Ho Nyeong <dev.honing@gmail.com>
2024-01-02 23:27:59 +0900

    Fix: JSDoc의 함수 설명 수정

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 /**
-* 에디터에 내장된 JSDoc 주석 방식으로 함수를 설명한다.
-
-  @param {string} elementNode - HTML 요소의 이름을 나타내는 문자열
-  @param {object} attributes - HTML 요소의 속성을 나타내는 객체
-  @param {array} children - 자식 엘리먼트로 구성된 배열
-  @returns {string} - 구성된 HTML 문자열
-  
-  @example
-  component{
-    "div".
-    {style: "color: blue;"},
-    [component("hi", {}, ["This is Page 1"]
-  )];
-}
-*/
+ * JSDoc 주석을 사용하여 함수를 설명한다.
+ *
+ * @param {string} elementNode - HTML 요소의 이름을 나타내는 문자열
+ * @param {object} attributes - HTML 요소의 속성을 나타내는 객체
+ * @param {array} children - 자식 엘리먼트로 구성된 배열
+ * @returns {string} - 구성된 HTML 문자열
+ * 
+ * @example
+ * component(
+ *   "div",
+ *   { style: "color: blue;" },
+ *   [component("hi", {}, ["This is Page 1"])]
+ * );
+ */
 
 function component(elementNode, attributes, children){
   
