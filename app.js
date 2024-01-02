2aadf0f50b190ab935a11a35b2eabd553b02ec64
Ho Nyeong <dev.honing@gmail.com>
2024-01-02 23:26:46 +0900

    Feat: HTML 컴포넌트를 생성하는 함수 구현
    - 요소 이름, 속성, 자식 엘리먼트를 받아 구성된 HTML 문자열을 반환함
    - for in 문 소개

--- app.js
+++ app.js
@@ -17,4 +17,14 @@
 
 function component(elementNode, attributes, children){
   
+  // HTML Element 문자열로 '조립(assemble)'하는 패턴
+  let elementStr = `<${elementNode}`;
+  for (let key in attributes) { // 객체의 키(key)를 배열처럼 순회한다.
+    // 매개변수 attributes는 객체여야 for in 문을 사용할 수 있다.
+    // 대표적으로 Python에서는 해당 문법 접근이 기본 반복문이다.
+    elementStr += `${key} = "${attributes[key]}"`;
+  }
+  elementStr += '>';
+  // 만약 children이라는 값이 '있다면' true 판정이 이루어진다.
+  // 조건식에서 "존재유무"로도 사용하기도 한다.
 }
